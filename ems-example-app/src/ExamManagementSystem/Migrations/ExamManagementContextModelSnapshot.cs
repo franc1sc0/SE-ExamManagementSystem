8ee01cf6281901ba8aca53d837cc4bbc1c272be9
Olaf Alexander <o_a1@txstate.edu>
2016-04-19 01:07:55 -0500

    Fleshed out Exam Models

---
+++ ems-example-app/src/ExamManagementSystem/Migrations/ExamManagementContextModelSnapshot.cs
@@ -0,0 +1,62 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using ExamManagementSystem.Models;
+
+namespace ExamManagementSystem.Migrations
+{
+    [DbContext(typeof(ExamManagementContext))]
+    partial class ExamManagementContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ExamManagementSystem.Models.Exam", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Date");
+
+                    b.Property<DateTime>("EndTime");
+
+                    b.Property<string>("Location");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTime>("RegDeadline");
+
+                    b.Property<string>("Semester");
+
+                    b.Property<DateTime>("StartTime");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("ExamManagementSystem.Models.Faculty", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+                });
+
+            modelBuilder.Entity("ExamManagementSystem.Models.Student", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+                });
+        }
+    }
+}
--- ems-example-app/src/ExamManagementSystem/Models/Exam.cs
+++ ems-example-app/src/ExamManagementSystem/Models/Exam.cs
@@ -9,7 +9,17 @@ namespace ExamManagementSystem.Models
 {
     public class Exam
     {
+        [ScaffoldColumn(false)]
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime Date { get; set; }
+        [Display(Name="Start Time")]
+        public DateTime StartTime { get; set; }
+        [Display(Name = "End Time")]
+        public DateTime EndTime { get; set; }
+        [Display(Name = "Registration Deadline")]
+        public DateTime RegDeadline { get; set; }
+        public string Semester { get; set; }
+        public string Location { get; set; }
     }
 }
