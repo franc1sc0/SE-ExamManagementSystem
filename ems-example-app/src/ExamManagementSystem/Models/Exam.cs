de4a3c06ea4669c704ec94fd295f227522b4475e
Olaf Alexander <o_a1@txstate.edu>
2016-04-19 23:57:06 -0500

    Updated Exam Model, removed database migrations, fleshed out Exam Views

--- ems-example-app/src/ExamManagementSystem/Models/Exam.cs
+++ ems-example-app/src/ExamManagementSystem/Models/Exam.cs
@@ -11,15 +11,30 @@ namespace ExamManagementSystem.Models
     {
         [ScaffoldColumn(false)]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}",ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
+        [Required]
+        [DataType(DataType.Time)]
         [Display(Name="Start Time")]
         public DateTime StartTime { get; set; }
+        [Required]
+        [DataType(DataType.Time)]
         [Display(Name = "End Time")]
         public DateTime EndTime { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         [Display(Name = "Registration Deadline")]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime RegDeadline { get; set; }
+        [Required]
         public string Semester { get; set; }
+        [Required]
         public string Location { get; set; }
     }
 }
