d2b65ec57e58ae6757c6d59a568a7d2ba024919f
Francisco Diocares <FranciscoDiocares@Gmail.com>
2016-04-21 23:39:39 -0500

    commented out seed data & fixed faculty link

--- ems-example-app/src/ExamManagementSystem/Models/ExamManagementContextSeedData.cs
+++ ems-example-app/src/ExamManagementSystem/Models/ExamManagementContextSeedData.cs
@@ -54,29 +54,29 @@ namespace ExamManagementSystem.Models
 
             if (!_context.Faculty.Any())
             {
-                var faculty1 = new Faculty();
-                faculty1.Name = "Professor1";
+                //var faculty1 = new Faculty();
+                //faculty1.Name = "Professor1";
 
-                _context.Add(faculty1);
-                _context.SaveChanges();
+                //_context.Add(faculty1);
+                //_context.SaveChanges();
             }
 
             if (!_context.Students.Any())
             {
-                var student1 = new Student();
-                student1.Name = "Student1";
+                //var student1 = new Student();
+                //student1.Name = "Student1";
 
-                _context.Add(student1);
-                _context.SaveChanges();
+                //_context.Add(student1);
+                //_context.SaveChanges();
             }
 
             if (!_context.Exams.Any())
             {
-                var exam1 = new Exam();
-                exam1.Name = "Exam1";
+                //var exam1 = new Exam();
+                //exam1.Name = "Exam1";
 
-                _context.Add(exam1);
-                _context.SaveChanges();
+                //_context.Add(exam1);
+                //_context.SaveChanges();
             }
         }
     }
