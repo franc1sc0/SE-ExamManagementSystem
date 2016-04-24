0a35a9cebeef02bdc5982b7074dd83f2b5f2227d
Francisco Diocares <FranciscoDiocares@Gmail.com>
2016-04-24 04:56:16 -0500

    adding Student
    removing superfluous toggle
    renaming RegExams

---
+++ ems-example-app/src/ExamManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,112 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using ExamManagementSystem.Models;
+
+namespace ExamManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private ExamManagementContext _context;
+
+        public StudentController(ExamManagementContext context)
+        {
+            _context = context;    
+        }
+
+
+        // GET: Student/Details/5
+        public async Task<IActionResult> Details()
+        {
+            var currentUserName = User.Identity.Name; 
+            Student student = await _context.Students.SingleAsync(m => m.UserName == currentUserName);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Student/Create
+        public IActionResult Exams()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Student/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Student student = await _context.Students.SingleAsync(m => m.studentID == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Student/Delete/5
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            Student student = await _context.Students.SingleAsync(m => m.studentID == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Student student = await _context.Students.SingleAsync(m => m.studentID == id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
